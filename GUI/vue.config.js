a485fc417549862604644ae16c73b58a22a72c91
Jonas Müller-Laackman <jonas.mueller-laackman@fu-berlin.de>
2022-05-11 15:15:46 +0200

    add new build

--- GUI/vue.config.js
+++ GUI/vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/Closing-The-Gap-In-Non-Latin-Script-Data/" : "/",
 })
