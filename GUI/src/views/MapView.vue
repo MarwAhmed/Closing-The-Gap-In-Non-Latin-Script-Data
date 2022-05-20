40dda7b9fd3c6cada65385d1cd7b0d490311f6a3
Jonas Müller-Laackman <jonas.mueller-laackman@fu-berlin.de>
2022-05-20 10:53:27 +0200

    add icons to map

--- GUI/src/views/MapView.vue
+++ GUI/src/views/MapView.vue
@@ -34,6 +34,16 @@ export default defineComponent({
     const route = useRoute();
     let map = null;
     const markers = L.markerClusterGroup();
+    const svgIcon = L.divIcon({
+        html: `
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+      </svg>`,
+        className: "",
+        iconAnchor: [10, 20],
+        popupAnchor: [0, -20],
+      });
+
 
     const resetData = () => {
       projectList.value = [];
@@ -52,7 +62,19 @@ export default defineComponent({
                   source: `https://github.com/Closing-the-Gap-in-NLS-DH/Projects/blob/master${responseIndex.data[key].path}${key}.json`,
                 });
                 responseProject.data.project.places.map((p) => {
-                  markers.addLayer(L.marker([p.coordinates.lat, p.coordinates.lng]));
+                  const marker = L.marker([p.coordinates.lat, p.coordinates.lng], { icon: svgIcon });
+                  marker.bindPopup(`
+                  <p class="flex flex-row">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                    </svg> <span class="mt-1">${p.place_name.text}</span>
+                  </p>
+                  <p class="text-xl mb-">${responseProject.data.project.title}</p>
+                  <ul>
+                    <li>Link: <a target="_blank" href="${responseProject.data.project.websites[0]}">${responseProject.data.project.websites[0]}</a></li>
+                    <li>Source: <a target="_blank" href="https://github.com/Closing-the-Gap-in-NLS-DH/Projects/blob/master${responseIndex.data[key].path}${key}.json">GitHub</a>
+                  `).openPopup();
+                  markers.addLayer(marker);
                 })
 
                 responseProject.data.project.keywords.map((tag) => {
